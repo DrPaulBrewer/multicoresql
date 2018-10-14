c6f7acc875e5f4ef66eee216cf5f7648ff53f5e6
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2018-10-14 14:29:19 -0400

    replace gig->giga in suite()

--- test/test1G.py
+++ test/test1G.py
@@ -51,31 +51,31 @@ if os.system(setupsqls):
 
 
 def suite(mybin,db):
-    m0 = "select n from gig where n>=1000 and n<=2000;"
+    m0 = "select n from giga where n>=1000 and n<=2000;"
     r0 = "select sum(n) from maptable;"
     e0 = (2000*2001/2)-(999*1000/2)
     t0 = 1
     test(mybin,db,m0,r0,e0,t0)
 
-    m1 = "select sum(n) as sn from gig;"
+    m1 = "select sum(n) as sn from giga;"
     r1 = "select sum(sn) from maptable;"
     e1 = 1000000000*1000000001/2
     t1 = 1
     test(mybin,db,m1,r1,e1,t1)
     
-    m2 = "select sum(1.0/(1.0+((1.0e-9)*n))) as z from gig;"
+    m2 = "select sum(1.0/(1.0+((1.0e-9)*n))) as z from giga;"
     r2 = "select (1.0e-9)*sum(z) as ln2 from maptable;"
     e2 = math.log(2.0)
     t2 = 0.00001
     test(mybin,db,m2,r2,e2,t2)
     
-    m3 = "select sum( (1.0/(4.0*n+1.0))-(1.0/(4.0*n-1.0)) ) as pisum from gig;"
+    m3 = "select sum( (1.0/(4.0*n+1.0))-(1.0/(4.0*n-1.0)) ) as pisum from giga;"
     r3 = "select 4.0*(1.0+sum(pisum)) as LeibnizPI from maptable;"
     e3 = math.pi
     t3 = 0.00001
     test(mybin,db,m3,r3,e3,t3)
     
-    m4 = "select sum( (1.0/(n*n)) ) as recip2sum from gig;"
+    m4 = "select sum( (1.0/(n*n)) ) as recip2sum from giga;"
     r4 = "select sum(recip2sum) as EulerPi2Over6 from maptable;"
     e4 = math.pi*math.pi/6.0
     t4 = 0.00001
